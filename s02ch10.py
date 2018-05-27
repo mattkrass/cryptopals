a56fe2d4d136a2908e2174c9e369de9a2ba58642
Matt Krass <mkrass1@bloomberg.net>
2018-05-27 15:16:45 -0400

    cleanup and iv generation extraction

--- s02ch10.py
+++ s02ch10.py
@@ -2,21 +2,23 @@
 import cryptopals
 
 key = b'YELLOW SUBMARINE'
-initial = cryptopals.pcksPad(b'Alicia Brown')
-encrypted = cryptopals.encryptAESDataECB(initial, key)
-encryptedCBC = cryptopals.encryptAESDataCBC(initial, key)
-decrypted = cryptopals.decryptAESDataECB(encrypted, key)
-decryptedCBC = cryptopals.decryptAESDataCBC(encryptedCBC, key)
+initial = cryptopals.pcksPad(b'Loki the cat')
+blockSize = 16
+iv = bytes([ 0 ] * blockSize)
+encryptedECB = cryptopals.encryptAESDataECB(initial, key)
+encryptedCBC = cryptopals.encryptAESDataCBC(initial, key, iv)
+decryptedECB = cryptopals.decryptAESDataECB(encryptedECB, key)
+decryptedCBC = cryptopals.decryptAESDataCBC(encryptedCBC, key, iv)
 
 print('Initial       = %s' % (initial,))
-print('Encrypted     = %s' % (encrypted,))
+print('Encrypted ECB = %s' % (encryptedECB,))
 print('Encrypted CBC = %s' % (encryptedCBC,), end=' ')
-cryptopals.test(encrypted, encryptedCBC)
-print('Decrypted     = %s' % (decrypted,), end = ' ')
-cryptopals.test(decrypted, initial)
+cryptopals.test(encryptedECB, encryptedCBC)
+print('Decrypted ECB = %s' % (decryptedECB,), end = ' ')
+cryptopals.test(decryptedECB, initial)
 print('Decrypted CBC = %s' % (decryptedCBC,), end = ' ')
 cryptopals.test(decryptedCBC, initial)
 
 cipherdata = cryptopals.loadBase64File('10.txt')
-plaindata = cryptopals.decryptAESDataCBC(cipherdata, key)
+plaindata = cryptopals.decryptAESDataCBC(cipherdata, key, iv)
 print('plaindata = %s' % (plaindata.decode('utf-8'),))
